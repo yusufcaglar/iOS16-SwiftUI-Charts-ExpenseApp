1767ed1ef84fc89c0e0464f5c717de6dbd65f1c4
Yusuf Çağlar <faf7faf7@gmail.com>
2022-09-16 14:43:39 +0300

    Initial Commit

---
+++ BudgetingApp/BudgetingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetingAppApp.swift
+//  BudgetingApp
+//
+//  Created by Yusuf Çağlar on 16.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct BudgetingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
